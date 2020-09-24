c6f50e33901dc15e253eeea4bd3c177ce908bd70
balbino aylagas <balbino_a@hotmail.com>
2020-09-23 23:36:04 -0600

    clase esp operadores

---
+++ 04-clase-esp-operadores/operadores.py
@@ -0,0 +1,93 @@
+# Operadores
+
+# # aritmeticos
+# x = 5
+# y = 2
+
+# print(x + y)  # suma
+# print(x - y)  # resta
+# print(x * y)  # multiplicacion
+# print(x / y)  # division
+# print(x % y)  # modulo
+# print(x ** y)  # exponenciacion
+# print(x // y)  # floor division
+
+# # asignacion
+# x = 5
+# y = 2
+# x += y  # suma x = x+y
+# print(x)
+
+# x = 5
+# y = 2
+# x -= y  # resta x = x-y
+# print(x)
+
+# x = 5
+# y = 2
+# x *= y  # multiplicacion
+# print(x)
+
+# x = 5
+# y = 2
+# x /= y  # division
+# print(x)
+
+# x = 5
+# y = 2
+# x %= y  # division
+# print(x)
+
+# x = 5
+# y = 2
+# x //= y  # floor division x = x//y
+# print(x)
+
+# x = 5
+# y = 2
+# x **= y  # exponenciacion x = x**y
+# print(x)
+
+# los operadores de comparasion
+"""
+== igual
+!= no igual
+> mayor que
+< menor que
+>= mayor o igual
+<= menor o igual
+"""
+# x = 5
+# y = 2
+# if x > y:
+#     print("x es mayor")
+# else:
+#     print("y es mayor")
+
+# operadores logicos
+"""
+and y logico
+or o logico
+not la negacion
+"""
+# x = 4
+# if not x == 2:
+#     print("oh happy day")
+
+# operadores de identidad
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+
+# print(x is not y)
+# print(x is not z)
+
+# operadores de membership (contiene)
+"""
+in
+not in
+"""
+# x = "12345"
+# print("123" in x)
+# print("45" not in x)
+# print("9" not in x)
