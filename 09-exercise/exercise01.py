3e89feadb7eb74e67198592a490f32a277c12daa
balbino aylagas <balbino_a@hotmail.com>
2020-09-25 20:21:48 -0600

    exercise 01 v02

--- 09-exercise/exercise01.py
+++ 09-exercise/exercise01.py
@@ -1,7 +1,38 @@
 """
-quiero escribir un programa python que reciba de input del usuario
-un nombre, un producto, costo
-y se guarde en una lista con diccinarios, el registro es un diccionario
-y va a ser guardado en una lista
+cliente:
+    quiero escribir un programa python que reciba de input del usuario
+    un nombre, un producto, costo
+    y se guarde en una lista con diccinarios, el registro es un diccionario
+    y va a ser guardado en una lista
+programador:
+    vaya
+
+cliente:
+    mira me gusta pero cada vez que inicio el programa, me borra el cliente anterior
+    y solo puedo guardar el que he ingresado en ese momento, no habria forma de
+    que se guarde a los 3 clientes que tengo?
+programador:
+    vaya
+mente:
+    y pollo no queres?
+    
 
 """
+
+# creando la lista vacia
+listaRegistro = []
+# cliente = input("nombre del cliente: ")
+# producto = input("producto: ")
+# costo = float(input("costo ($0.00): "))
+
+cliente = "pepito"
+producto = "brownie"
+costo = 3.0
+
+# punto de programa
+# registro = {"cliente": cliente, "producto": producto, "costo": costo}
+registro = dict(cliente=cliente, producto=producto, costo=costo)
+# como agrego un elemento nuevo a una lista?
+listaRegistro.append(registro)
+# dejar de ocupar la variable registro
+# registro = None
