0a2000db660ec013f885bf6a175746b3f53d7764
balbino aylagas <balbino_a@hotmail.com>
2020-09-25 19:37:11 -0600

    exercise 01

---
+++ 09-exercise/exercise01.py
@@ -0,0 +1,7 @@
+"""
+quiero escribir un programa python que reciba de input del usuario
+un nombre, un producto, costo
+y se guarde en una lista con diccinarios, el registro es un diccionario
+y va a ser guardado en una lista
+
+"""
