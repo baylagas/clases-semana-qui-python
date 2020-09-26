63d276675b0548139e6d70c2be26426b3437f3ca
balbino aylagas <balbino_a@hotmail.com>
2020-09-25 20:52:33 -0600

    exercise 01 v03

--- 09-exercise/exercise01.py
+++ 09-exercise/exercise01.py
@@ -15,24 +15,37 @@ programador:
     vaya
 mente:
     y pollo no queres?
-    
+
+cliente:
+    mira esta buenisimo peeeerroooooo, fijate que mi clientela a aumentado a veces tengo
+    3 a veces 5  a veces 10 osea ya no se cuantos clientes tendre en un dia, podrias
+    agregarle alguna forma de que yo le diga cuando quiero que se detenga y que me muestre
+    el reporte de lo que llevo en costo total hasta ese momento.
+programador:
+    vaya
+mente:
+    no te pide nada el gusto mono
+
 
 """
 
 # creando la lista vacia
 listaRegistro = []
-# cliente = input("nombre del cliente: ")
-# producto = input("producto: ")
-# costo = float(input("costo ($0.00): "))
+clientes = 0
 
-cliente = "pepito"
-producto = "brownie"
-costo = 3.0
+while clientes < 3:
+    cliente = input("nombre del cliente: ")
+    producto = input("producto: ")
+    costo = float(input("costo ($0.00): "))
 
-# punto de programa
-# registro = {"cliente": cliente, "producto": producto, "costo": costo}
-registro = dict(cliente=cliente, producto=producto, costo=costo)
-# como agrego un elemento nuevo a una lista?
-listaRegistro.append(registro)
-# dejar de ocupar la variable registro
-# registro = None
+    # punto de programa
+    # registro = {"cliente": cliente, "producto": producto, "costo": costo}
+    registro = dict(cliente=cliente, producto=producto, costo=costo)
+    # como agrego un elemento nuevo a una lista?
+    listaRegistro.append(registro)
+    # dejar de ocupar la variable registro
+    # registro = None
+    clientes += 1
+
+for registro in listaRegistro:
+    print(registro)
