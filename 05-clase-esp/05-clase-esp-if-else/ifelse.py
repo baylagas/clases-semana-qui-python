89383e5e6b83d3b4817924febf98ed6e1349fb11
baylagas <balbino_a@hotmail.com>
2020-09-29 06:23:41 -0600

    clase esp if else

---
+++ 05-clase-esp/05-clase-esp-if-else/ifelse.py
@@ -0,0 +1,89 @@
+# IF ELSE
+
+"""
+Python supports the usual logical conditions from mathematics:
+
+Equals:                     a == b
+Not Equals:                 a != b
+Less than:                  a < b
+Less than or equal to:      a <= b
+Greater than:               a > b
+Greater than or equal to:   a >= b
+"""
+
+# a = 2
+# b = 5
+# if a>b:
+#     print("a es mayor que b")
+# print("buenos dias")
+
+# elif
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+
+# else
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+# short hand if
+# a = 200
+# b = 33
+# if a > b: print("a is greater than b")
+
+# short hand if else
+# a = 200
+# b = 33
+# print("a") if a>b else print("b")
+
+# a = 30
+# b = 33
+# print("a") if a>b else print("=") if a==b else print("b")
+
+# and
+# a = 200
+# b = 33
+# c = 500
+# if a>b and c>a:
+#   print("both conditions are True")
+
+# or
+# a = 200
+# b = 33
+# c = 500
+# if a > c or a < b:
+#   print("At least one of the conditions is True")
+
+# nested if
+# x =41
+# if x>10:
+#   print("above ten, ")
+#   if x>20:
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.")
+
+# pass statement
+# a = 33
+# b = 200
+
+# if b>a:
+#   pass
+# else:
+#   pass
