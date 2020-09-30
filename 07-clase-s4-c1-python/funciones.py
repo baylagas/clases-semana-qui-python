bc50c4e6f63da3b2941d00f68ecd8d82ddcb0fdd
baylagas <balbino_a@hotmail.com>
2020-09-29 18:44:47 -0600

    funciones v01

---
+++ 07-clase-s4-c1-python/funciones.py
@@ -0,0 +1,39 @@
+# funciones
+
+# programacion procedural
+"""
+este paradigma lo que hace es que nos permite poder programar por medio de pasos logicos
+y secuenciales de inicio a fin
+
+"""
+# programacion funcional
+"""
+este paradigma lo que hace es que nos permite programar por medio de funciones que hacen una
+o varias acciones
+
+"""
+# programacion orientada a objetos
+"""
+este paradigma nos permite poder programar nuestros sistemas simulando objetos dentro de la vida
+real, traducidos aun ambiente digital
+
+    objeto en la vida real
+        propiedades
+            las variables (definen algo de un objeto)
+        acciones
+            funciones (realizar acciones)
+"""
+
+# funciones
+# es un bloque de codigo que se puede mandar a llamar (con un nombre)
+# y a este bloque se le puede enviar informacion en forma de parametros
+# pero tambien este puede regresar valores por medio de su valor de retorno
+
+
+# # es la definicion de una funcion
+# def mifuncion():
+#     print("hola mundo")
+
+
+# # la llamada de una funcion
+# mifuncion()
