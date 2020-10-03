6a9f30dc2e6069b3608ab11272cf8ff1d525486f
baylagas <balbino_a@hotmail.com>
2020-10-02 18:37:03 -0600

    clases y objetos v01

---
+++ 08-clase-s4-c2-python/ejemplo-clases.py
@@ -0,0 +1,6 @@
+from person import Person
+
+person1 = Person("balbino", 23)
+print(person1)
+print(person1.name)
+print(person1.age)
---
+++ 08-clase-s4-c2-python/person.py
@@ -0,0 +1,9 @@
+# la clase person
+# a esta clase le creamos el metodo constructor __init__()
+# que recibe 2 parametros el nombre y la edad
+# este metodo crea dos variables de instancia llamadas name y age
+class Person:
+    # metodo constructor de la clase
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
