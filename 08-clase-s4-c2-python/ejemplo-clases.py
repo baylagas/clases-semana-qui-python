0bc112e2665bcb2b2b9cd93872f5bdfc13ecf63b
baylagas <balbino_a@hotmail.com>
2020-10-02 19:01:58 -0600

    clases y objetos v02

--- 08-clase-s4-c2-python/ejemplo-clases.py
+++ 08-clase-s4-c2-python/ejemplo-clases.py
@@ -1,6 +1,17 @@
-from person import Person
+# from <archivo py> import <clase, metodo, variable>
+from person import Person, Factory, Operation
+
 
 person1 = Person("balbino", 23)
-print(person1)
+# print(person1)
 print(person1.name)
 print(person1.age)
+
+factory1 = Factory()
+factory1.createCar()
+
+operation1 = Operation(2, 4)
+result = operation1.add()
+print(result)
+result = operation1.multiply()
+print(result)
--- 08-clase-s4-c2-python/person.py
+++ 08-clase-s4-c2-python/person.py
@@ -2,8 +2,56 @@
 # a esta clase le creamos el metodo constructor __init__()
 # que recibe 2 parametros el nombre y la edad
 # este metodo crea dos variables de instancia llamadas name y age
+"""
+clases en base a sus propiedades y acciones
+"""
+
+
+"""
+hay clases que solo tienen propiedades (variables)
+    su proposito es solo guardar informacion
+"""
+
+
 class Person:
     # metodo constructor de la clase
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
+"""
+hay clases que solo tiene acciones (metodos)
+    su proposito es solo procesar informacion, otras clases, clases trabajadoras
+
+"""
+
+
+class Factory:
+    def createCar(self):
+        print("creating car...")
+
+    def createComputer(self):
+        pass
+
+    def createAirplane(self):
+        pass
+
+
+"""
+hay clases mas comunes que tienen ambos= guardan informacion y tienen metodos que procesan
+    esa informacion
+
+"""
+
+
+class Operation:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+
+    def add(self):
+        return self.number1 + self.number2
+
+    def multiply(self):
+        return self.number1 * self.number2
